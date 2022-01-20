ec46c381060d852d1cc01baf7a3ef75fa74f53b2
Jéssica Florentino <jdgflorentino@gmail.com>
2022-01-19 22:53:19 -0300

    Requisito 6.

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -27,14 +27,35 @@ const productDetails = require('../src/productDetails');
 */
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
+	let product = productDetails('Alcool', 'Mascara');
+  it('Verifica se productDetails é uma função.', () => {
     // Teste se productDetails é uma função.
+		expect(typeof productDetails).toBe('function');
+	}),
     // Teste se o retorno da função é um array.
-    // Teste se o array retornado pela função contém dois itens dentro.
-    // Teste se os dois itens dentro do array retornado pela função são objetos.
+	it('Verifica se a função `productDetails` retorna um array.', () => {
+			expect(product).toEqual(expect.any(Array));
+	}),
+	it('Verifica se a função `productDetails` retorna um array.', () => {
+		expect(product).toBeInstanceOf(Array);
+  }),
+	// Teste se o array retornado pela função contém dois itens dentro.
+	it('Verifica se o array retornado pela função contém dois itens dentro.', () => {
+		expect(product.length).toBe(2);
+  }),
+	// Teste se os dois itens dentro do array retornado pela função são objetos.
+	it('Verifica se os dois itens dentro do array retornado pela função são objetos', () => {
+		expect(typeof product[0] === 'object' && typeof product[1] === 'object' ).toEqual(true);
+  }),
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+	it('Verifica se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si', () => {
+		expect(product[0] !== product[1]).toEqual(true);
+	});
     // Teste se os dois productIds terminam com 123.
-  });
+	it('Verifica se dois productIds terminam com 123.', () => {
+		str1 = product[0].details.productId;
+		str2 = product[1].details.productId;
+		expect(str1.slice(str1.length -3) && str2.slice(str2.length -3)).toMatch('123');
+	});
 });
+
